bf8b6cb957086a017846027203a6718cd2b22a9c
zhuxucome <zhuxucome@126.com>
2022-08-15 11:13:13 +0800

    第一次提交应用

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+/*
+ * @Author: zhuxucome zhuxucome@126.com
+ * @Date: 2022-08-14 15:07:34
+ * @LastEditors: zhuxucome zhuxucome@126.com
+ * @LastEditTime: 2022-08-15 11:06:38
+ * @FilePath: \my-app\src\App.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import logo from './logo.svg';
 import './App.css';
+
+function sayHi() {
+  alert('hi')
+}
 
 function App() {
   return (
@@ -7,7 +19,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          王强读书天才计划
         </p>
         <a
           className="App-link"
@@ -15,8 +27,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          把所有时间都用来读书，疯狂zpy
         </a>
+        <button onClick={function(){alert('奋斗，才是真正的幸福')}}>点我</button>
       </header>
     </div>
   );
